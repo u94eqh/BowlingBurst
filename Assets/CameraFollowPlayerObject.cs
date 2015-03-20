63788d2236ac81e96a853d3e2bfbc210c38ac5fc
Jordan <u94eqh@live.ca>
2015-03-20 15:01:05 -0300

    Initial commit

---
+++ Assets/CameraFollowPlayerObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayerObject : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+	public bool gutterFlag = false;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (gutterFlag == false) {
+			transform.position = player.transform.position + offset;
+		} else {
+			transform.position = transform.position;
+		}
+	}
+}
---
+++ Assets/ballMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballMovement : MonoBehaviour {
+	bool gutterFlag = false;
+	float gutterPos1 = 5.5f;
+	float gutterPos2 = -5.5f;
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Vector3 jump = new Vector3 (0.0f, 500.0f, 0.0f);
+
+		if (GetComponent<Rigidbody> ().velocity.magnitude < 50 && Physics.Raycast (transform.position, -Vector3.up, 1) && (gutterFlag == false)) {
+			gutterFlag = false;
+			GetComponent<Rigidbody> ().AddForce (movement * 20);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && Physics.Raycast (transform.position, -Vector3.up, 1) && gutterFlag == false) {
+			gutterFlag = false;
+			GetComponent<Rigidbody>().AddForce (jump);
+		}
+
+		//if (Physics.Raycast (transform.position, -Vector3.up, 1) && gutterFlag == false) {
+		//	gutterFlag = true;
+		//}
+
+		//if (gutterFlag == true) {
+		//	GetComponent<CameraFollowPlayerObject>().gutterFlag = true;
+		//}
+	}
+}
